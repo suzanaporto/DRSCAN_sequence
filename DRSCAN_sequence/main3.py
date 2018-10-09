d7a46a1ed484cf60e9919689b35576d9a1bb405f
suzanaporto <43450225+suzanaporto@users.noreply.github.com>
2018-10-09 16:47:44 -0300

    Update main3.py

--- DRSCAN_sequence/main3.py
+++ DRSCAN_sequence/main3.py
@@ -319,14 +319,14 @@ def main(args):
 def txt_2_list(return_list, filename = "sequenciasdef.txt"):
 
     if(return_list):
-	  f = open(filename,"r")
-	  txt_to_list =[]
-	  for line in f:
+	f = open(filename,"r")
+	txt_to_list =[]
+	for line in f:
 		txt_to_list.append()
-	  f.close
-	  return txt_to_list
+	f.close
+	return txt_to_list
     else: 
-	  return False			
+	return False			
 	
 
 if __name__ == '__main__':
