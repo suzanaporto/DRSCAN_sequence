8cf4721868f4f73fc522f4c114c3c2483e1a4e45
suzanaporto <43450225+suzanaporto@users.noreply.github.com>
2018-10-09 15:40:31 -0300

    Add files via upload

---
+++ DRSCAN_sequence/sequence_gen.py
@@ -0,0 +1,24 @@
+#generate sequences
+import main3 as m3
+import pandas as pd
+
+
+def gen_sequence(snp_name, info_list):
+	res = []
+	for i in info_list:
+		
+		snp_location = i['location']
+		snp_chrom = i['chrom']
+		snp_al = i['allele_wt']
+		snp_al_v = i['allele_v']
+
+		args = {"snp_name":snp_name,"snp_location":snp_location,"snp_chrom":snp_chrom,"snp_al_wt":snp_al,"snp_al_v":snp_al_v,'return_list':True}
+		
+		res.extend(m3.main(args))
+	
+	print(res)
+		
+	#TODO res to dataframe
+	df = pd.read_csv(res, sep='\t')
+	return df;
+	
