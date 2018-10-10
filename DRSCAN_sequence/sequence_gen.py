d007d2f701bf0581a254fb5c86c1a91219885043
suzanaporto <43450225+suzanaporto@users.noreply.github.com>
2018-10-10 15:46:49 -0300

    Update sequence_gen.py

--- DRSCAN_sequence/sequence_gen.py
+++ DRSCAN_sequence/sequence_gen.py
@@ -11,15 +11,18 @@ def gen_sequence(snp_name, info_list):
 		snp_chrom = i['chrom']
 		snp_al = i['allele_wt']
 		snp_v = i['allele_v']
+		verf = "n"
+		del_s = "n"
+		type_s = "SNV"
 
 		args = {"snp_name":snp_name,
 			"snp_location":snp_location,
 			"snp_chrom":snp_chrom,
 			"snp_al_wt":snp_al,
 			"snp_al_v":snp_v,
-			"snp_charac":"SNV",
-			"verify":"n",
-			"delete_snp":"n",
+			"snp_charac":type_s,
+			"verify":verf,
+			"delete_snp":del_s,
 			'return_list':True}
 		
 		print(args['snp_name'])
