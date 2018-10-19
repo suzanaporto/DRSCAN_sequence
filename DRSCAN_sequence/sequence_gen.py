ce0e90fd47ef6f65b301f3cd393939f81638ac40
suzanaporto <43450225+suzanaporto@users.noreply.github.com>
2018-10-19 16:04:10 -0300

    Update sequence_gen.py

--- DRSCAN_sequence/sequence_gen.py
+++ DRSCAN_sequence/sequence_gen.py
@@ -31,7 +31,7 @@ def gen_sequence(snp_name, info_list,genome_version):
 				"snp_chrom":snp_chrom,
 				"snp_al_wt":snp_al,
 				"snp_al_v":snp_al_v,
-				'return_list':True
+				'return_list':True,
 				"genome_version": genome_version}
 		
 		res.extend(m3.main(args))
