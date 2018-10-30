b82ad53d8f1636779c9d4c6d6655362a1adb2f27
suzanaporto <43450225+suzanaporto@users.noreply.github.com>
2018-10-30 15:03:45 -0300

    Update main3.py

--- DRSCAN_sequence/main3.py
+++ DRSCAN_sequence/main3.py
@@ -55,91 +55,19 @@ def main(args):
 	#----------------------Isso aqui dentro de um while para poder pegar as várias snps do usuario------------------///
 
 	#for i in range(numero_de_snps):
-	#pedir para o usuario inserir uma snp(nome da snp)
-	snp_nome = args['snp_name']
-	snp_local = int(args['snp_location'])
-	#alguns dos atributos para o alelo
-	snp_chrom = int(args['snp_chrom'])
-	snp_charact = args['snp_charac']
-	snp_ancestral =  args['snp_al_wt']
-	minor_allele =  args['snp_al_v']
 	genome_version = args['genome_version']
 	minor_allele.upper()
 
-	allele_comum_insert = Allele(nome=snp_ancestral,local=snp_local,cromossomo=snp_chrom,is_comum=True,snp_pos=0)
-
-	for i in minor_allele:
-		if i == 'A' or i == 'C' or i == 'G' or i == 'T':
-			minor_allele_insert = Allele(nome=i,local=snp_local,cromossomo=snp_chrom,is_comum=False,snp_pos=0)
-			minor_allele_list.append(minor_allele_insert)
-
-	#cria um objeto do tipo snp e insere as informações da snp nele
-	snp_insert = Snp(name=snp_nome, location=snp_local, chrom=snp_chrom,
-                charact=snp_charact,ancestral_al= allele_comum_insert, minor_al=minor_allele_list)
-
-	#coloca a snp na lista de snps
-	lista_snp.append(snp_insert)
-
-	#deleta tudo da lista de minor alleles
-	minor_allele_list = []
+    lista_snp = args['snp_list']
 
 	'''
 	allele1 = Allele(nome='T',local=17282721,cromossomo=19,is_comum=True,snp_pos=0)
 	allele2 = Allele(nome='A',local=17282721,cromossomo=19,is_comum=False,snp_pos=0)
 	allele3 = Allele(nome='A',local=36662856,cromossomo=14,is_comum=True,snp_pos=0)
-	allele4 = Allele(nome='G',local=36662856,cromossomo=14,is_comum=False,snp_pos=0)
-	allele5 = Allele(nome='C',local=204515489,cromossomo=1,is_comum=True,snp_pos=0)
-	allele6 = Allele(nome='G',local=204515489,cromossomo=1,is_comum=False,snp_pos=0)
-	allele7 = Allele(nome='T',local=56909835,cromossomo=5,is_comum=True,snp_pos=0)
-	allele8 = Allele(nome='A',local=56909835,cromossomo=5,is_comum=False,snp_pos=0)
-	allele9 = Allele(nome='G',local=56909835,cromossomo=5,is_comum=False,snp_pos=0)
-	allele10 = Allele(nome='T',local=36647076,cromossomo=14,is_comum=True,snp_pos=0)
-
-	allele11 = Allele(nome='C',local=36647076,cromossomo=14,is_comum=False,snp_pos=0)
-	allele12 = Allele(nome='C',local=27970429,cromossomo=12,is_comum=True,snp_pos=0)
-	allele13 = Allele(nome='G',local=27970429,cromossomo=12,is_comum=False,snp_pos=0)
-	allele14 = Allele(nome='G',local=105147856,cromossomo=4,is_comum=True,snp_pos=0)
-	allele15 = Allele(nome='T',local=105147856,cromossomo=4,is_comum=False,snp_pos=0)
-	allele16 = Allele(nome='C',local=54969306,cromossomo=17,is_comum=True,snp_pos=0)
-	allele17 = Allele(nome='A',local=54969306,cromossomo=17,is_comum=False,snp_pos=0)
-	allele18 = Allele(nome='G',local=21494705,cromossomo=10,is_comum=True,snp_pos=0)
-	allele19 = Allele(nome='A',local=21494705,cromossomo=10,is_comum=False,snp_pos=0)
-	allele20 = Allele(nome='C',local=36647066,cromossomo=14,is_comum=True,snp_pos=0)
-	allele21 = Allele(nome='A',local=36647066,cromossomo=14,is_comum=False,snp_pos=0)
-	allele22 = Allele(nome='T',local=18438074,cromossomo=19,is_comum=True,snp_pos=0)
-	allele23 = Allele(nome='C',local=18438074,cromossomo=19,is_comum=False,snp_pos=0)
-	allele24 = Allele(nome='A',local=13712867,cromossomo=6,is_comum=True,snp_pos=0)
-	allele25 = Allele(nome='G',local=13712867,cromossomo=6,is_comum=False,snp_pos=0)
-	allele26 = Allele(nome='G',local=52546588,cromossomo=16,is_comum=True,snp_pos=0)
-	allele27 = Allele(nome='A',local=52546588,cromossomo=16,is_comum=False,snp_pos=0)
-	allele28 = Allele(nome='C',local=21520483,cromossomo=10,is_comum=True,snp_pos=0)
-	allele29 = Allele(nome='T',local=21520483,cromossomo=10,is_comum=False,snp_pos=0)
-	allele30 = Allele(nome='T',local=56919325,cromossomo=5,is_comum=True,snp_pos=0)
-	allele31 = Allele(nome='C',local=56919325,cromossomo=5,is_comum=False,snp_pos=0)
 
 	snp1 = Snp(name='rs8108174',location=17282721,chrom=19,charact='snv',ancestral_al=allele1,minor_al=[allele2])
 	snp2 = Snp(name='rs12883049',location=36662856,chrom=14,charact='snv',ancestral_al=allele3,minor_al=[allele4])
-	snp3 = Snp(name='rs4951392',location=204515489,chrom=1,charact='snv',ancestral_al=allele5,minor_al=[allele6])
-	snp4 = Snp(name='rs252923',location=56909835,chrom=5,charact='snv',ancestral_al=allele7,minor_al=[allele8, allele9])
-
-	snp5 = Snp(name='rs72679711',location=36647076,chrom=14,charact='snv',ancestral_al=allele10,minor_al=[allele11])
-	snp6 = Snp(name='rs141089797',location=27970429,chrom=12,charact='snv',ancestral_al=allele12,minor_al=[allele13])
-	snp7 = Snp(name='rs62331150',location=105147856,chrom=4,charact='snv',ancestral_al=allele14,minor_al=[allele15])
-
-	snp8 = Snp(name='rs7208403',location=54969306,chrom=17,charact='snv',ancestral_al=allele16,minor_al=[allele17])
-
-	snp9 = Snp(name='rs12770228',location=21494705,chrom=10,charact='snv',ancestral_al=allele18,minor_al=[allele19])
-	snp10 = Snp(name='rs67155512',location=36647066,chrom=14,charact='snv',ancestral_al=allele20,minor_al=[allele21])
-	snp11 = Snp(name='rs2303696',location=18438074,chrom=19,charact='snv',ancestral_al=allele22,minor_al=[allele23])
-	snp12 = Snp(name='rs405447',location=13712867,chrom=6,charact='snv',ancestral_al=allele24,minor_al=[allele25])
-
-	snp13 = Snp(name='rs3095602',location=52546588,chrom=16,charact='snv',ancestral_al=allele26,minor_al=[allele27])
-
-	snp14 = Snp(name='rs34853448',location=21520483,chrom=10,charact='snv',ancestral_al=allele28,minor_al=[allele29])
-	snp15 = Snp(name='rs832533',location=56919325,chrom=5,charact='snv',ancestral_al=allele30,minor_al=[allele31])
-
-
-	lista_snp = [snp1,snp2,snp3,snp4,snp5,snp6,snp7,snp8,snp9,snp10,snp11,snp12,snp13,snp14,snp15]
+	
 	'''
 	# Perguntar se o usuário gostaria de saber se as snps estão em um elemento regulatório baseado no roadmap epigenomics
 
@@ -220,7 +148,6 @@ def main(args):
 				print (str(len(lista_por_chrom)))
 				#verifica se a lista de cromossomo for maior do que 1
 				if len(lista_por_chrom) > 1:
-					print ("Entrou aqui")
 					#faz uma iteração na lista de cromossomos até o penúltimo
 					for i in range(len(lista_por_chrom)-1):
                     
@@ -300,12 +227,7 @@ def txt_2_list(return_list, filename = "sequenciasdef.fna"):
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser(description='')
-	parser.add_argument('--snp_name', default="rs268", type=str)
-	parser.add_argument('--snp_location', default=231322, type=int)
-	parser.add_argument('--snp_chrom', default=11, type=int)
-	parser.add_argument('--snp_charac', default='SNV', type=str)
-	parser.add_argument('--snp_al_wt', default='A', type=str)
-	parser.add_argument('--snp_al_v', default='C', type=str)
+	parser.add_argument('--snp_list', default=[], type=list)
 	parser.add_argument('--verify', default='n', type=str)
 	parser.add_argument('--delete_snp', default='n', type=str)
 	parser.add_argument('--return_list', default=False, type=bool)
