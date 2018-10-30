c2c70f66a23dc618be0a066de0b58eb2c1d12c5c
suzanaporto <43450225+suzanaporto@users.noreply.github.com>
2018-10-30 16:02:57 -0300

    Update sequence_gen.py

--- DRSCAN_sequence/sequence_gen.py
+++ DRSCAN_sequence/sequence_gen.py
@@ -16,11 +16,14 @@ def gen_sequence(snp_names, info_list,genome_version):
 	snp_list_object = []
 	minor_allele_list = []
 	res = []
+	
+	count = 0
+	
 	for i,snp_name in zip(info_list,snp_names):
-		snp_location = i['location']
-		snp_chrom = i['chrom']
-		snp_al = i['allele_wt']
-		snp_al_v = i['allele_v']
+		snp_location = i[count]['location']
+		snp_chrom = i[count]['chrom']
+		snp_al = i[count]['allele_wt']
+		snp_al_v = i[count]['allele_v']
 		
 		allele_comum_insert = Allele(nome=snp_al,local=snp_location,cromossomo=snp_chrom,is_comum=True,snp_pos=0)
 		
@@ -36,7 +39,9 @@ def gen_sequence(snp_names, info_list,genome_version):
 	
 		#coloca a snp na lista de snps
 		snp_list_object.append(snp_insert)
-	
+		
+		count+=1
+		
 	args = {"snp_list":snp_list_object,
 		"return_list":True,
 		"verify":'n',
@@ -45,4 +50,3 @@ def gen_sequence(snp_names, info_list,genome_version):
 	res.append(m3.main(args))
 	#print(res)
 	return res
-	
