f25742dcbd78fc1ae721c2ae7fba56ffdd2325a2
suzanaporto <43450225+suzanaporto@users.noreply.github.com>
2018-10-31 16:57:58 -0300

    Update snpDAO.py

--- DRSCAN_sequence/snpDAO.py
+++ DRSCAN_sequence/snpDAO.py
@@ -97,58 +97,59 @@ class SnpDAO (object):
             f.write(seq_meio_alt + '\n')  #writes o/p to add.txt file
             f.close()
 
-
     def request_sequence_combinations(self,lista_comb,genome,first_alleles=[],last_alleles=[],lista_de_alelos=[],lista_de_comb_sets=[],cont=1):
-	g = GraphDAO()
-	pos_relativa_lista = []
-	#ver as snps da lista de combinacoes e coloca os seus alelos na lista de alelos com numeros para representar as suas posicoes e snps
-	for j in lista_comb:
-		j.ancestral_al.snp_pos = cont
-		lista_de_alelos.append(j.ancestral_al)
-		for x in j.minor_al:
-			x.snp_pos = cont
-			lista_de_alelos.append(x)
-		cont +=1
-	#coloca os primeiros alelos numa lista e os ultimos alelos em outra lista
-	for y in lista_de_alelos:
-		if y.snp_pos == 1:
-			first_alleles.append(y)
-		else:
-			if y.snp_pos == lista_de_alelos[-1].snp_pos:
-				ultima_pos_da_snp = lista_de_alelos[-1].snp_pos
-				last_alleles.append(y)
-	#criar grafo com a lista de alelos devidamente preenchida/por algum motivo ele precisou do numero da ultima posicao
-	grafo = g.create_graph(lista_de_alelos,ultima_pos_da_snp)
-	
-	#fazer as combinacoes
-	for k in first_alleles:
-		for f in last_alleles:
-			lista_de_comb_sets.append(g.find_all_paths(grafo,start=k,end=f))
-	
-	#fazer as sequencias
-	first_location_snp = lista_comb[0].location
-	first_location_chrom = lista_comb[0].chrom
-	last_location_snp = lista_comb[-1].location
-	last_location_chrom = lista_comb[-1].location
-	#sequencia buscada do ensembl.org
-	request_text_middle = self.request_sequence_middle(first_location_snp,last_location_snp,first_location_chrom,genome)
-	for combinacao in lista_de_comb_sets:
-		for j in combinacao:
-			#quantidade de elementos
-			for l in range(len(j)):
-				if (l == 0):
-					#index do primeiro elemento
-					real_index = 50
-				else:
-					#calculo do index real
-					real_index = real_index + lista_comb[l].location - lista_comb[l-1].location
-					print("É pra ser o index real da snp2: " + str(real_index+1))
-					pos_relativa_lista.append(str(real_index+1))
-					request_text_middle = request_text_middle[:real_index] + j[l] + request_text_middle[real_index+1:]
-			string_dos_alelos = '|'.join(j)
-			string_nomes_snps = '|'.join(map(str, lista_comb))
-			pos_relativa_lista = '|'.join(pos_relativa_lista)
-			#string_nomes_snps = '|'.join(str(lista_comb))
+        g = GraphDAO()
+        pos_relativa_lista = []
+        #ver as snps da lista de combinacoes e coloca os seus alelos na lista de alelos com numeros para representar as suas posicoes e snps
+        for j in lista_comb:
+            j.ancestral_al.snp_pos = cont
+            lista_de_alelos.append(j.ancestral_al)
+            for x in j.minor_al:
+                x.snp_pos = cont
+                lista_de_alelos.append(x)
+            cont +=1
+            
+        #coloca os primeiros alelos numa lista e os ultimos alelos em outra lista
+        for y in lista_de_alelos:
+            if y.snp_pos == 1:
+                first_alleles.append(y)
+            else:
+                if y.snp_pos == lista_de_alelos[-1].snp_pos:
+                    ultima_pos_da_snp = lista_de_alelos[-1].snp_pos
+                    last_alleles.append(y)
+        
+        #criar grafo com a lista de alelos devidamente preenchida/por algum motivo ele precisou do numero da ultima posicao
+        grafo = g.create_graph(lista_de_alelos,ultima_pos_da_snp)
+        
+        #fazer as combinacoes
+        for k in first_alleles:
+            for f in last_alleles:
+                lista_de_comb_sets.append(g.find_all_paths(grafo,start=k,end=f))
+                
+        #fazer as sequencias
+        first_location_snp = lista_comb[0].location
+        first_location_chrom = lista_comb[0].chrom
+        last_location_snp = lista_comb[-1].location
+        last_location_chrom = lista_comb[-1].location
+        #sequencia buscada do ensembl.org
+        request_text_middle = self.request_sequence_middle(first_location_snp,last_location_snp,first_location_chrom,genome)
+        for combinacao in lista_de_comb_sets:
+            for j in combinacao:
+                #quantidade de elementos
+                for l in range(len(j)):
+                    if (l == 0):
+                        #index do primeiro elemento
+                        real_index = 50
+                    else:
+                        #calculo do index real
+                        real_index = real_index + lista_comb[l].location - lista_comb[l-1].location
+                        print("É pra ser o index real da snp2: " + str(real_index+1))
+                        pos_relativa_lista.append(str(real_index+1))
+                        request_text_middle = request_text_middle[:real_index] + j[l] + request_text_middle[real_index+1:]
+                string_dos_alelos = '|'.join(j)
+                string_nomes_snps = '|'.join(map(str, lista_comb))
+                pos_relativa_lista = '|'.join(pos_relativa_lista)
+                #string_nomes_snps = '|'.join(str(lista_comb))
 			print (">sequence_combinations|"+ string_nomes_snps + "|" + str(lista_comb[0].chrom) + "|" +string_dos_alelos+ "|" + pos_relativa_lista)
 			print(request_text_middle)
 			f = open("sequenciasdef.fna","w")   #create add file in write mode
@@ -159,11 +160,11 @@ class SnpDAO (object):
 			#colocas só as sequencias nessa lista
 			#lista_de_sequencias.append(request_text_middle)
 			#criar objetos para colocá-los numa lista de sequencias
-	#deletar as listas
-	real_index = 0
-	cont = 1
-	del first_alleles[:]
-	del last_alleles[:]
-	del lista_comb[:]
-	del lista_de_alelos[:]
-	del lista_de_comb_sets[:]
+	    #deletar as listas
+	    real_index = 0
+	    cont = 1
+        del first_alleles[:]
+        del last_alleles[:]
+        del lista_comb[:]
+        del lista_de_alelos[:]
+        del lista_de_comb_sets[:]
