ecd44162dc437b85d69a11a9b4bf143a3c8e8b4c
suzanaporto <43450225+suzanaporto@users.noreply.github.com>
2018-11-01 16:58:27 -0300

    Update snpDAO.py

--- DRSCAN_sequence/snpDAO.py
+++ DRSCAN_sequence/snpDAO.py
@@ -98,7 +98,7 @@ class SnpDAO (object):
             f.write(">sequence_variation|"+str(snp.name) +"|"+str(snp.chrom)+"|"+j.nome+"|"+str((snp.location-x)+starting_at_one)+ '\n')
             f.write(seq_meio_alt + '\n')  #writes o/p to add.txt file
         f.close()
-	return False
+return False
 
     def request_sequence_combinations(self,lista_comb,genome,is_first,first_alleles=[],last_alleles=[],lista_de_alelos=[],lista_de_comb_sets=[],cont=1):
         g = GraphDAO()
