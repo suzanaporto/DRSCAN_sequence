ae33b8f7f1f7d9f531d308095c4a95bced6c6b74
suzanaporto <43450225+suzanaporto@users.noreply.github.com>
2018-11-01 14:24:05 -0300

    Update snpDAO.py

--- DRSCAN_sequence/snpDAO.py
+++ DRSCAN_sequence/snpDAO.py
@@ -146,7 +146,8 @@ class SnpDAO (object):
                     print("É pra ser o index real da snp2: " + str(real_index+1))
                     pos_relativa_lista.append(str(real_index+1))
                     request_text_middle = request_text_middle[:real_index] + j[l] + request_text_middle[real_index+1:]
-                string_dos_alelos = '|'.join(j)
+                print(j)
+		string_dos_alelos = '|'.join(j)
                 string_nomes_snps = '|'.join(map(str, lista_comb))
                 pos_relativa = '|'.join(pos_relativa_lista)
                 #string_nomes_snps = '|'.join(str(lista_comb))
